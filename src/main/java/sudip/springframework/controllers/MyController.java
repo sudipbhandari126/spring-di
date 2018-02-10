cd5fa380b33668f2d7e6d2674b126762c7965727
Sudip Bhandari <sudip.b@axisrooms.com>
2018-02-10 11:30:27 +0530

    initial commit: spring core DI

---
+++ src/main/java/sudip/springframework/controllers/MyController.java
@@ -0,0 +1,16 @@
+package sudip.springframework.controllers;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by jt on 5/23/17.
+ */
+@Controller
+public class MyController {
+
+    public String hello(){
+        System.out.println("Hello!!! ");
+
+        return "foo";
+    }
+}
